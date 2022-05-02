c068d4f75f7f4034e80b91c7d74c73f0a0181175
Gotsmy <mathias.gotsmy@univie.ac.at>
2022-05-02 10:45:53 +0200

    Added check for bounds in the objective function.

--- snek_utils.py
+++ snek_utils.py
@@ -1,18 +1,35 @@
 import cobra
+import warnings
+'''
+The functions listed here are wrappers to important cobra functions that do additional checks:
+* ensuring correct spelling,
+* check for logical errors.
+'''
 
 def set_objective(model,reaction,direction='max'):
     '''
-    Updates the objective function of a model.
+    Updates the objective function of a model. Also checks if the objective reaction have unusual bounds. 
+    I.e. something else than (lower bound, upper bound)
+    * -1000/1000 for maximization and minimization,
+    *     0/1000 for maximization, or
+    * -1000/   0 for minimization.
     -
     Input:
         model       cobrapy model
         reaction    objective ration ID
-        direction   objective direction. default = 'max'
+        direction   objective direction. either 'min' or 'max'. default = 'max'
     -
     Output:
         model       updated cobrapy model
     '''
     
+    if model.reactions.get_by_id(reaction).upper_bound == 1000 and model.reactions.get_by_id(reaction).lower_bound == -1000:
+        pass
+    elif direction == 'min' and (model.reactions.get_by_id(reaction).lower_bound != -1000 or model.reactions.get_by_id(reaction).upper_bound != 0):
+        warnings.warn('The objective reaction has a lower bound at {:.2f} and a upper bound at {:.2f}. This could lead to problems during optimization.'.format(model.reactions.get_by_id(reaction).lower_bound,model.reactions.get_by_id(reaction).upper_bound))
+    elif direction == 'max' and (model.reactions.get_by_id(reaction).lower_bound != 0 or model.reactions.get_by_id(reaction).upper_bound != 1000):
+        warnings.warn('The objective reaction has a lower bound at {:.2f} and a upper bound at {:.2f}. This could lead to problems during optimization.'.format(model.reactions.get_by_id(reaction).lower_bound,model.reactions.get_by_id(reaction).upper_bound))
+
     model.objective = reaction
     model.objective_direction = direction
     return model
