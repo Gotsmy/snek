925981b44496def7139ab79107d1e8f5a17cff99
Mathias Gotsmy <mathias.gotsmy@univie.ac.at>
2022-07-13 14:27:11 +0200

    added test for in_flux().

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -7,6 +7,8 @@ model = cobra.io.load_model("textbook")
 c_fluxes = snek.analysis.carbon_fluxes(model)
 assert len(c_fluxes) == 3
 
-
+# check snek.analysis.in_flux
+in_flux = snek.analysis.in_flux(model)
+assert in_flux['EX_glc__D_e'] == -10
 
 print('End of test_analysis.py')
