b335f9983e353ffeceee53bbc15e8c93a101327b
Mathias Gotsmy <mathias.gotsmy@univie.ac.at>
2022-07-15 11:16:12 +0200

    Added fixtures to tests.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,19 +1,21 @@
+import pytest
 import cobra
 import snek
 
-def test_carbon_fluxes():
+@pytest.fixture
+def model():
+    model = cobra.io.load_model("textbook")
+    return model
+
+# check snek.analysis.carbon_fluxes
+def test_carbon_fluxes(model):
     model = cobra.io.load_model("textbook")
     c_fluxes = snek.analysis.carbon_fluxes(model)
     assert len(c_fluxes) == 3
 
-def test_in_flux():
-    model = cobra.io.load_model("textbook")
+# check snek.analysis.in_flux
+def test_in_flux(model):
     in_flux = snek.analysis.in_flux(model)
     assert in_flux['EX_glc__D_e'] == -10
-
-# check snek.analysis.carbon_fluxes
-test_carbon_fluxes()
-# check snek.analysis.in_flux
-test_in_flux()
 
 print('End of test_analysis.py')
--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,20 +1,30 @@
-import cobra
-import snek
-from Bio import SeqUtils
+import pytest
 import numpy as np
+import cobra
+from Bio import SeqUtils
+import snek
 
+@pytest.fixture
+def model():
+    model = cobra.io.load_model("textbook")
+    return model
+
+# check snek.elements.count_atom
 def test_count_atom():
     nr = snek.elements.count_atom('C16H15N3.99','N')
     assert nr == 3.99
 
+# check snek.elements.element_composition
 def test_element_composition():
     all = snek.elements.element_composition('C16H15N3.99')
     assert all == {'C':16,'H':15,'N':3.99}
 
+# check snek.elements.unique_elements
 def test_unique_elements():
     elements = snek.elements.unique_elements('C16H15N3.99')
     assert len(elements) == 3
 
+# check snek.elements.sum_formula_pDNA
 def test_sum_formula_pDNA():
     # test if my two functions work as intended and compare results to the Bio.SeqUtils.molecular_weight function.
     # the big difference: with my functions I also get the sum formula of the pDNA
@@ -25,22 +35,16 @@ def test_sum_formula_pDNA():
     # assert that the error is smaller than 0.1
     assert np.abs((test_mw_my_implementation-test_mw_bio)/np.mean([test_mw_bio,test_mw_my_implementation])*100) < .1
 
-def test_element_fluxes():
-    model = cobra.io.load_model("textbook")
+# check snek.elements.element_fluxes
+def test_element_fluxes(model):
     C_fluxes = snek.elements.element_fluxes(model,'C')
     assert C_fluxes.loc['EX_glc__D_e','C_flux'] == 60
     H_fluxes = snek.elements.element_fluxes(model,'H')
     assert np.isclose(H_fluxes.loc['EX_h2o_e','H_flux'],-58.351654271131565,atol=model.tolerance,rtol=0)
 
-# check count_atom
-test_count_atom()
-# check element_composition
-test_element_composition()
-# check unique_elements
-test_unique_elements()
-# check molecular_weight and sum_formula_pDNA
-test_sum_formula_pDNA()
-# check element_fluxes
-test_element_fluxes()
+# check snek.elements.element_flux_coefficient
+def test_element_flux_coefficient(model):
+    c_flux_coefficient = snek.elements.element_flux_coefficient(model,'C','EX_glc__D_e')
+    assert c_flux_coefficient == -6
 
 print('End of test_elements.py')
