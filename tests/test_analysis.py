25d99af80b36c2e7253752389706f2ecfd1b9981
Mathias Gotsmy <mathias.gotsmy@univie.ac.at>
2022-07-15 11:50:38 +0200

    Updated test_analysis.py according to removed functions.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -7,12 +7,6 @@ def model():
     model = cobra.io.load_model("textbook")
     return model
 
-# check snek.analysis.carbon_fluxes
-def test_carbon_fluxes(model):
-    model = cobra.io.load_model("textbook")
-    c_fluxes = snek.analysis.carbon_fluxes(model)
-    assert len(c_fluxes) == 3
-
 # check snek.analysis.in_flux
 def test_in_flux(model):
     in_flux = snek.analysis.in_flux(model)
